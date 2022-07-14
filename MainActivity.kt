1c8b2a449a25d49dd016364c6b9e71d0dc9aa2b5
A-Jamong <106497478+A-Jamong@users.noreply.github.com>
2022-07-14 21:08:35 +0900

    Add files via upload
    MainActivity.kt = 가장 첫 화면 로그인 화면
    signup_page.kt = 회원가입 화면
    activity_signup_page.xml = 회원가입 화면 (아직 만드는중)
    activity_main = 로그인 화면
    box_round_rectangular.xml = 로그인 화면 버튼 이미지

---
+++ MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.trpg_try
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bt_signup = findViewById<button>(R.id.signupbt)
+        bt_signup.
+
+    }
+}
