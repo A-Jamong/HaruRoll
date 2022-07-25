fbda3176aba46bda24b184bc70dfdd701950dfc3
RiiuL <riul5627k@gmail.com>
2022-07-26 00:43:16 +0900

    setup

--- app/src/main/java/com/example/trpg_try/MainActivity.kt
+++ app/src/main/java/com/example/trpg_try/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.trpg_try
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var id:EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -12,4 +15,5 @@ class MainActivity : AppCompatActivity() {
         // 이제 할 일 : 버튼 누르면 다음 화면 넘어가게
 
     }
+
 }
