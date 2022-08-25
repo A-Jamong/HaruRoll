116187abcba8c7194b83993e6c427f266a0a1fd1
jaemyung <99.1.Some@gmail.com>
2022-08-25 21:41:06 +0900

    main_session 추가 string.xml 추가

---
+++ app/src/main/java/com/example/trpg_try/main_session.kt
@@ -0,0 +1,12 @@
+package com.example.trpg_try
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class main_session : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_session)
+    }
+}
+
