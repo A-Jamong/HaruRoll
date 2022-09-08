f65a36a4d3dbf884c7d9e9e2ee1b6a9d29a66748
jamong <99.1.Some@gmail.com>
2022-09-08 19:08:30 +0900

    이미지 불러오기 기능 추가중

--- app/src/main/java/com/example/trpg_try/main_session.kt
+++ app/src/main/java/com/example/trpg_try/main_session.kt
@@ -1,12 +1,18 @@
 package com.example.trpg_try
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.main_session.*
 
 class main_session : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_session)
+        session_select_01.setOnClickListener{
+            val intent = Intent(this, roadimage::class.java)
+            startActivity(intent)
+        }
     }
 }
 
--- app/src/main/java/com/example/trpg_try/roadimage.kt
+++ app/src/main/java/com/example/trpg_try/roadimage.kt
@@ -1,11 +1,86 @@
 package com.example.trpg_try
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.roadimage.*
+import java.io.InputStream
+import java.util.jar.Manifest
 
 class roadimage : AppCompatActivity() {
+    private var uri: Uri? =null
+    val FLAG_REQ_STORAGE = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.roadimage)
+        img_bt.setOnClickListener{
+            checkPermission()
+        }
+        button2.setOnClickListener {
+            sessionimg.setImageResource(R.drawable.box_rectangular)
+        }
     }
+    private fun checkPermission(){
+        val imagePermission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        if(imagePermission ==PackageManager.PERMISSION_GRANTED){
+            openGallery()
+        }else{
+            requestPermission()
+        }
+    }
+    fun onRequestPermissionResult(
+        requestCode: Int,
+        permission: Array<out String>,
+        grantResult: IntArray
+    ){
+        when(requestCode){
+            99->{
+                if(grantResult[0]==PackageManager.PERMISSION_GRANTED){
+                    openGallery()
+                }else{
+                    Toast.makeText(this, "승인을 거절했습니다", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+        }
+    }
+    private fun openGallery(){
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent,FLAG_REQ_STORAGE)
+        Toast.makeText(this, "갤러리를 실행합니다.", Toast.LENGTH_LONG).show()
+    }
+    private fun requestPermission(){
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 99)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode:Int, data: Intent?){
+        super.onActivityResult(
+            requestCode,
+            resultCode,
+            data
+        )
+        if(requestCode==RESULT_OK){
+            when(requestCode){
+                FLAG_REQ_STORAGE->{
+                    uri = data?.data
+                    sessionimg.setImageURI(uri)
+                }
+            }
+        }else{
+        Toast.makeText(this, "?", Toast.LENGTH_SHORT).show()}
+    }
+
+
 }
