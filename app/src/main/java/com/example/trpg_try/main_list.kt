f5df145144a0afe36d7cb25834d9fd9d051ad71b
jamong <99.1.Some@gmail.com>
2022-12-29 18:41:13 +0900

    캐릭터 생성창으로 넘어가게

--- app/src/main/java/com/example/trpg_try/main_list.kt
+++ app/src/main/java/com/example/trpg_try/main_list.kt
@@ -1,12 +1,20 @@
 package com.example.trpg_try
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.main_list.*
 
 class main_list : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_list)
 
+
+
+        bt_newchar.setOnClickListener{
+            val intent = Intent(this@main_list, make_char::class.java)
+            startActivity(intent)
+        }
     }
 }
