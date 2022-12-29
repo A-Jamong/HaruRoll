8ab9de6c2044ff8258d60babe36f0cd460c38985
jamong <99.1.Some@gmail.com>
2022-12-29 18:21:58 +0900

    로그인 시 열리는 창 변경

--- app/src/main/java/com/example/trpg_try/MainActivity.kt
+++ app/src/main/java/com/example/trpg_try/MainActivity.kt
@@ -4,22 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.view.View
-import android.widget.Button
 import com.example.trpg_try.api.AppSessionKey
 import com.example.trpg_try.api.Login
 import com.example.trpg_try.api.send_Login
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_signup_page.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.coroutineScope
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.util.logging.Logger
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,7 +41,7 @@ class MainActivity : AppCompatActivity() {
                     //통신 성공했을 때 화면 넘어가게
                     if (login?.code.equals("0000")) {
                         AppSessionKey = login?.AppSessionKey!!
-                        val intent = Intent(this@MainActivity, main_session::class.java)
+                        val intent = Intent(this@MainActivity, main_list::class.java)
                         startActivity(intent)
                     }
                     else {
